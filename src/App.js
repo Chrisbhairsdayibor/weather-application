9aa7837d3a9b260ee45e8c6ef1a303b093e12412
Chrisbhairsdayibor <127002088+Chrisbhairsdayibor@users.noreply.github.com>
2024-10-22 11:40:36 +0000

    Weather Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {useState}from 'react';
 
-function App() {
+
+const api ={
+    key:"fe721e0f26e07b99defde3e6527132bf",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
+
+export default function App() {
+const [search, setsearch] = useState("");
+const [weather,setWeather] = useState({});
+
+
+const searchPressed = () =>{
+fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+.then(res => res.json())
+.then(result => {
+    setWeather(result); 
+});
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header className="App-header">
+        {/* Header */}
+      <h1>Weather App</h1>
+
+      {/* search Box */}
+      <div>
+<input type="text" placeholder="Enter city/town..."
+onChange={(e)=>setsearch(e.target.value)}
+/>
+<button onClick={searchPressed}>Search</button>
+</div>
+
+{/* If weather is not undefined */}
+{typeof weather.main !== "undefined" ?(
+    <div>
+       {/* Location */}
+      <p>{weather.name}</p>
+
+      {/* Temperature F/C */}
+<p>{weather.main.temp}°C </p>
+      {/* Conndition (sunny) */}
+      
+      <p>{weather.weather[0].main}</p>
+      <p>({weather.weather[0].descritions})</p>
+      </div>
+):(
+""
+)}
       </header>
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Another method
+
+
+// import React, {useState,useEffect} from 'react'
+
+// export default function App() {
+//   const [weather, setWeather] = useState()
+//   const [city, setCity] = useState()
+
+//   const api ={
+//     key:"fe721e0f26e07b99defde3e6527132bf",
+//     base: "https://api.openweathermap.org/data/2.5/",
+// }
+
+
+
+//   useEffect(() => {
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+//         .then(response => response.json())
+//         .then(json => setWeather(json))
+//   }, [city,apikey]);
+  
+  
+//   return (
+//     <div className='App'>
+//       <pre>{JSON.stringify(weather, null, 2)}</pre>
+//       <input type='text' onChange={(e)=>{setCity(e.target.value)
+
+//       }}
+//       value={city}
+//       />
+      
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+
